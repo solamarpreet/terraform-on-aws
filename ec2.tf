b247f469961a990a5e06cac7a7965ea995b62a94
Amarpreet Singh <solamarpreet@gmail.com>
2023-04-15 19:21:42 +0530

    simple ec2 done with user_data

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,18 @@
 resource "aws_instance" "test_instance" {
-  subnet_id = aws_subnet.test_subnet.id
-  instance_type = "t2.micro"
-  ami = data.aws_ami.ubuntu_ami_id.id
-  key_name = aws_key_pair.test_ssh_key.id
+  subnet_id                   = aws_subnet.test_subnet.id
+  instance_type               = "t2.micro"
+  ami                         = data.aws_ami.ubuntu_ami_id.id
+  key_name                    = aws_key_pair.test_ssh_key.id
   associate_public_ip_address = true
-  vpc_security_group_ids = [ aws_security_group.test_sg.id ]
-  user_data = <<-EOF
+  vpc_security_group_ids      = [aws_security_group.test_sg.id]
+  user_data                   = <<-EOF
+#!/bin/bash
+apt update && apt -y dist-upgrade
+apt install -y nginx
+echo "working" > /tmp/userdatacheck.txt
+EOF
 
-  apt update && apt -y dist-upgrade
-  apt install nginx
-  
-  EOF
+  user_data_replace_on_change = true
 }
 
 resource "aws_key_pair" "test_ssh_key" {
--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "test_vpc" {
-  cidr_block = "10.0.0.0/24"
+  cidr_block           = "10.0.0.0/24"
   enable_dns_hostnames = true
 
   tags = {
@@ -8,7 +8,7 @@ resource "aws_vpc" "test_vpc" {
 }
 
 resource "aws_subnet" "test_subnet" {
-  vpc_id = aws_vpc.test_vpc.id
+  vpc_id     = aws_vpc.test_vpc.id
   cidr_block = "10.0.0.0/24"
 }
 
@@ -18,10 +18,17 @@ resource "aws_internet_gateway" "test_internet_gw" {
 
 resource "aws_route_table" "test_route_table" {
   vpc_id = aws_vpc.test_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.test_internet_gw.id
-  }
+}
+
+resource "aws_route" "test_route" {
+  route_table_id         = aws_route_table.test_route_table.id
+  gateway_id             = aws_internet_gateway.test_internet_gw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "test_rt_a_1" {
+  subnet_id      = aws_subnet.test_subnet.id
+  route_table_id = aws_route_table.test_route_table.id
 }
 
 resource "aws_security_group" "test_sg" {
@@ -30,24 +37,24 @@ resource "aws_security_group" "test_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "test_http" {
   security_group_id = aws_security_group.test_sg.id
-  cidr_ipv4 = "0.0.0.0/0"
-  from_port = 80
-  to_port = 80
-  ip_protocol = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "test_ssh" {
   security_group_id = aws_security_group.test_sg.id
-  cidr_ipv4 = "0.0.0.0/0"
-  from_port = 22
-  to_port = 22
-  ip_protocol = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
 }
 
 resource "aws_vpc_security_group_egress_rule" "test_all_traffic" {
   security_group_id = aws_security_group.test_sg.id
-  cidr_ipv4 = "0.0.0.0/0"
-  from_port = -1
-  to_port = -1
-  ip_protocol = -1
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = -1
 }
